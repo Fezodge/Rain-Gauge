06c7854df3fb6113af7c495b6db4ab4d90ae457d
Joseph Pullan <j.pullan2000@gmail.com>
2016-01-29 20:33:58 +0000

    Added Counter
    I added a counter to the program, as this will be converted into the
    amount of rainfall.

--- Rain_Gauge/Rain_Gauge.ino
+++ Rain_Gauge/Rain_Gauge.ino
@@ -1,33 +1,26 @@
 #define LED 13
+int tipCount = 0;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   attachInterrupt(0, displayMicros, RISING);
-  attachInterrupt(1, displayMillis, RISING);
-  attachInterrupt(2, debounceInterrupt, RISING);
+  //attachInterrupt(1, debounceInterrupt, RISING);
 }
 
-void debounceInterrupt() {
+/*void debounceInterrupt() {
   if((long)(micros() - last_micros) >= debouncing_time * 1000 {
     Interrupt();
     last_micros = micros();
   }
-}
+}*/
 
 void displayMicros() {
   Serial.write("micros() = ");
   Serial.println(micros());
-}
-
-void displayMillis() {
-  Serial.write("millis() = ");
-  Serial.println(millis());
+  tipCount++;
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+  Serial.println(tipCount);
+  tipCount = 0;
   }
